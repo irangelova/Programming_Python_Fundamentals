359b06efe3819272e800f69a63d5ebeae3e1f5fe
irangelova <ivetarangelova@gmail.com>
2024-09-29 23:40:18 +0200

    Tasks solutions from List basics lecture

---
+++ list_basics/02_courses.py
@@ -0,0 +1,8 @@
+number_of_courses = int(input())
+courses = []
+
+for number in range(number_of_courses):
+    course = input()
+    courses.append(course)
+
+print(courses)
---
+++ list_basics/03_list_statistics.py
@@ -0,0 +1,18 @@
+count_numbers = int(input())
+
+positive_numbers = []
+negative_numbers = []
+
+for number in range(count_numbers):
+    current_number = int(input())
+
+    if current_number >= 0:
+        positive_numbers.append(current_number)
+    else:
+        negative_numbers.append(current_number)
+
+print(positive_numbers)
+print(negative_numbers)
+print(f"Count of positives: {len(positive_numbers)}")
+print(f"Sum of negatives: {sum(negative_numbers)}")
+
