bfa6c7180ff18c0a3e534507b0ef714450a154cd
irangelova <ivetarangelova@gmail.com>
2024-10-03 18:25:28 +0200

    update

---
+++ list_basics/E_07_easter_gifts.py
@@ -0,0 +1,29 @@
+gift_names = input().split()
+#print(gift_names)
+#command = input().split()
+#print(command)
+cleaned_gift_list = final_gift_list = []
+index = 0
+
+while True:
+    command_string = input().split()
+    command = command_string[0]
+    gift = command_string[1]
+    if len(command_string) == 3:
+        index = int(command_string[2])
+    if command == "OutOfStock":
+        for index, current_gift in enumerate(gift_names):
+            if current_gift == gift:
+                gift_names[index] = "None"
+    elif command == "Required" and 0 < index <= len(gift_names)-1:
+        gift_names[index] = gift
+    elif command == "JustInCase":
+        gift_names[-1] = gift
+    elif command == "No":
+        break
+
+for final_gift in gift_names:
+    if final_gift != "None":
+        cleaned_gift_list.append(final_gift)
+    final_gift_list = " ".join(cleaned_gift_list)
+print(final_gift_list)
---
+++ list_basics/test.py
@@ -0,0 +1,3 @@
+command_string = input().split()
+command = command_string[0]
+print(command)
