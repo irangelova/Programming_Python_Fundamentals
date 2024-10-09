be85dcc6d1b8f63a3d7a5a68b4b8605d2c565bdb
irangelova <ivetarangelova@gmail.com>
2024-10-09 23:10:38 +0200

    Tasks solutions from Functions lecture

---
+++ functions/E_01_smallest_of_three_numbers.py
@@ -0,0 +1,10 @@
+def find_smallest(number1, number2, number3):
+    numbers_to_check = [number1, number2, number3]
+    smallest_number = min(numbers_to_check)
+    return smallest_number
+
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+print(find_smallest(first_number, second_number, third_number))
