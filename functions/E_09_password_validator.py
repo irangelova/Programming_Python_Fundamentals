2cea5e1a005096a7156f043748d8b5145393cd41
irangelova <ivetarangelova@gmail.com>
2024-10-10 21:03:22 +0200

    Tasks solutions from Functions lecture

---
+++ functions/E_09_password_validator.py
@@ -0,0 +1,20 @@
+def password_validator(password: str) -> str:
+    error_messages = []
+    count_numbers = 0
+    if len(password) <6 or len(password) > 10:
+        error_messages.append("Password must be between 6 and 10 characters")
+    if not password.isalnum():
+        error_messages.append("Password must consist only of letters and digits")
+    for element in password:
+        if element.isdigit():
+            count_numbers += 1
+    if count_numbers < 2:
+        error_messages.append("Password must have at least 2 digits")
+    if not error_messages:
+        return "Password is valid"
+    return "\n".join(error_messages)
+
+
+password_to_check = input()
+result = password_validator(password_to_check)
+print(result)
