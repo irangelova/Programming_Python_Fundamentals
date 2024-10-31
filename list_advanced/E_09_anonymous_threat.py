12691e49b8b4e3ce0c82685ac2a24472c03b70d9
irangelova <ivetarangelova@gmail.com>
2024-10-31 18:48:02 +0100

    Solutions to tasks from List advanced lecture

--- list_advanced/E_09_anonymous_threat.py
+++ list_advanced/E_09_anonymous_threat.py
@@ -12,21 +12,36 @@ while True:
         end_index = int(command.split()[2])
 
         if 0 <= end_index <= len(input_message) - 1:
-            input_message[start_index:end_index] = ["".join(input_message[start_index:end_index])]
-        else:
-            input_message[start_index:(len(input_message) - 1)] = ["".join(input_message[start_index:(len(input_message) - 1)])]
+            input_message[start_index:(end_index + 1)] = ["".join(input_message[start_index:(end_index + 1)])]
+        elif start_index < 0:
+            input_message[0:end_index] = ["".join(input_message[0:(end_index + 1)])]
+        elif end_index > len(input_message) - 1:
+            input_message[start_index:len(input_message) + 1] = ["".join(input_message[start_index:len(input_message) + 1])]
+
     elif action == "divide":
+        counter = 0
+        total_partitions = 0
+        new_word = ""
         given_index = int(command.split()[1])
         partition = int(command.split()[2])
+        word_to_divide = input_message[given_index]
         length_of_partitions_at_given_index = len(input_message[given_index]) // partition
-        start_index = 0
-        for part in range(1, partition + 1):
-            if part == (partition + 1) and len(input_message[given_index]) % partition != 0:
-                current_part = input_message[given_index][start_index:]
+        input_message.pop(given_index)
+        for letter in word_to_divide:
+            counter += 1
+            if counter < 2 and total_partitions < partition - 1:
+                new_word += letter
+                continue
+            if counter == 2 and total_partitions < partition - 1:
+                new_word += letter
+                total_partitions += 1
+                input_message.append(new_word)
+                counter = 0
+                new_word = ""
+                continue
             else:
-                current_part = input_message[given_index][start_index:(start_index + length_of_partitions_at_given_index)]
-                start_index += length_of_partitions_at_given_index
+                new_word += letter
+        input_message.append(new_word)
 
 print(" ".join(input_message))
-
 
