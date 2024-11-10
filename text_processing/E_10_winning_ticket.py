d97a716994946d63068f220840aa4bf24bd321a3
irangelova <ivetarangelova@gmail.com>
2024-11-10 23:42:08 +0100

    Solutions to tasks from Text Processing lecture

---
+++ text_processing/E_10_winning_ticket.py
@@ -0,0 +1,23 @@
+def check_ticket(ticket: str) -> str:
+    if len(ticket) != 20:
+        return("invalid ticket")
+
+    winning_symbols = ["@", "#", "$", "^"]
+    first_half = ticket[:10]
+    second_half = ticket[10:]
+    for current_symbol in winning_symbols:
+        for uninterrupted_match in range(10, 5, -1):
+            winning_symbol_repetition = current_symbol * uninterrupted_match
+
+            if winning_symbol_repetition in first_half and winning_symbol_repetition in second_half:
+                if uninterrupted_match == 10:
+                    return f'ticket "{ticket}" - {uninterrupted_match}{current_symbol} Jackpot!'
+                return f'ticket "{ticket}" - {uninterrupted_match}{current_symbol}'
+    return f'ticket "{ticket}" - no match'
+
+
+all_tickets = input().split(", ")
+for ticket in all_tickets:
+    ticket_to_check = ticket.strip()
+    result = check_ticket(ticket_to_check)
+    print(result)
